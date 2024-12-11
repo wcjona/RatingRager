5f15a4800e0598e0409855db0544fcff08922634
Jonathan <jonathan.chong@ucalgary.ca>
2024-12-11 14:03:54 -0700

    Push Scores

--- data/tables.sql
+++ data/tables.sql
@@ -1,18 +1,28 @@
-CREATE TABLE restaurant_details (
+CREATE TABLE restaurants (
     id INTEGER PRIMARY KEY,
     name TEXT,
     rating REAL,
     rating_volume INTEGER,
     address TEXT,
     latitude REAL,
-    longitude REAL
+    longitude REAL,
+    price_level INTEGER,
 );
 
-CREATE TABLE restaurant_reviews (
+CREATE TABLE reviews (
     id INTEGER PRIMARY KEY,
     restaurant_id INTEGER,
     author_name TEXT,
     rating INTEGER,
     description TEXT,
     date DATETIME
+);
+
+CREATE TABLE scores (
+    restaurant_id INTEGER,
+    sentiment_score REAL,
+    trend_score REAL,
+    location_score REAL,
+    overall_rating REAL,
+    price_score REAL
 );
